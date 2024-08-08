26162ff2765d54b7206a3bda3f34fe3bae0ff435
hip1hip <honginpyo2100@gmail.com>
2024-08-08 17:24:44 +0900

    08월 08일 CSS, JS

---
+++ 0808/js/calc.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  const mybutton = document.getElementById('mybutton');
+  mybutton.addEventListener('click', calc);
+
+  const result = document.getElementById('result');
+  result.style.visibility = 'hidden'
+});
+
+function calc() {
+  const f = document.forms[0];
+  let result = Number(f.first.value) + Number(f.second.value);
+  document.getElementById('result').innerHTML = `<div style = 'color: red'>${result} </div>`;
+
+  document.getElementById('result').style.visibility = 'visible';
+}
+
