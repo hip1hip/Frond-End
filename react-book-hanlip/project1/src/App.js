9ab24624ba9ec6b987b137ba12b376bb128ee48c
hip1hip <honginpyo2100@gmail.com>
2024-09-19 21:27:36 +0900

    한입크기 리엑트 04 useEffect

--- react-book-hanlip/project1/src/App.js
+++ react-book-hanlip/project1/src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
-import {useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
 
 function App() {
-const [count, setCount] = useState(0);
-const handleSetCount = (value) => {
-  setCount(count + value);
-};
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트 ");
+    }
+
+  });
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText}></input>
+      </section>
       <section>
         <Viewer count={count} />
       </section>
